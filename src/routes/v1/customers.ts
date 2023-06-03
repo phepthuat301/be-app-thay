9eec55d53215e88e66037d11f958242f4bcca8dd
vandai2995 <vandai2995@gmail.com>
2023-06-03 10:28:07 +0700

    update

--- src/routes/v1/customers.ts
+++ src/routes/v1/customers.ts
@@ -8,6 +8,6 @@ router.post('/create', [checkJwt], create);
 router.post('/edit', [checkJwt], edit);
 router.get('/list', [checkJwt], getCustomerList);
 router.post('/list-by-name', [checkJwt], getCustomerListByName);
-router.delete('/delete', [checkJwt], deleteCustomer);
+router.delete('/delete/:id', [checkJwt], deleteCustomer);
 
 export default router;
