071388b14511765ec144658418730986f0295618
Nam Tran <nam.tran@heatmob.net>
2024-06-04 15:28:23 +0700

    update

--- src/orm/migrations/1717468159689-RemoveUserConstraint.ts
+++ src/orm/migrations/1717468159689-RemoveUserConstraint.ts
@@ -6,14 +6,14 @@ export class RemoveUserConstraint1717468159689 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
     // Add fields to the existing "users" table
     await queryRunner.query(
-      `ALTER TABLE "user" DROP CONSTRAINT users_email_key;`,
+      `ALTER TABLE "users" DROP CONSTRAINT users_email_key;`,
     );
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
     // Reverse the changes made in the "up" method
     await queryRunner.query(
-      `ALTER TABLE "user" ADD CONSTRAINT users_email_key UNIQUE (email);`,
+      `ALTER TABLE "users" ADD CONSTRAINT users_email_key UNIQUE (email);`,
     );
   }
 }
